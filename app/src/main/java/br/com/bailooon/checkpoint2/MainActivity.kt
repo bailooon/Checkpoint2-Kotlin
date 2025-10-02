e0e67fb2f75fbd1974f8687a32eff32b314719a9
bailooon <matheus@bailon.com.br>
2025-10-02 20:12:35 -0300

    implementação da LazyColumn

--- app/src/main/java/br/com/bailooon/checkpoint2/MainActivity.kt
+++ app/src/main/java/br/com/bailooon/checkpoint2/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -22,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.bailooon.checkpoint2.repository.getAllGames
 import br.com.bailooon.checkpoint2.ui.theme.Checkpoint2Theme
 
 class MainActivity : ComponentActivity() {
@@ -64,5 +67,12 @@ fun GamesScreen(modifier: Modifier = Modifier) {
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn() {
+            items(getAllGames()) {
+                Column() {
+                    Text(text = it.title)
+                }
+            }
+        }
     }
 }
