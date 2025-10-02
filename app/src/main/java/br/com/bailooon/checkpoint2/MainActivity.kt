f79be519b4c436b627e08a57dd582c16e85c1b49
bailooon <matheus@bailon.com.br>
2025-10-02 20:16:17 -0300

    Exibição dos games utilizando um Card

--- app/src/main/java/br/com/bailooon/checkpoint2/MainActivity.kt
+++ app/src/main/java/br/com/bailooon/checkpoint2/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,16 +16,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.bailooon.checkpoint2.components.GameCard
 import br.com.bailooon.checkpoint2.repository.getAllGames
 import br.com.bailooon.checkpoint2.ui.theme.Checkpoint2Theme
 
@@ -69,9 +75,7 @@ fun GamesScreen(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(16.dp))
         LazyColumn() {
             items(getAllGames()) {
-                Column() {
-                    Text(text = it.title)
-                }
+                GameCard(game = it)
             }
         }
     }
