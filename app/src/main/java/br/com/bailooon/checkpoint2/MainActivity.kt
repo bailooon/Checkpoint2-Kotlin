30731cd632e040c8fdbbca37ac1c4f662fcc9eca
bailooon <matheus@bailon.com.br>
2025-10-02 20:19:57 -0300

    Implementação da funcionalidade de busca

--- app/src/main/java/br/com/bailooon/checkpoint2/MainActivity.kt
+++ app/src/main/java/br/com/bailooon/checkpoint2/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,21 +14,23 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.bailooon.checkpoint2.components.GameCard
 import br.com.bailooon.checkpoint2.repository.getAllGames
+import br.com.bailooon.checkpoint2.repository.getGamesByStudio
 import br.com.bailooon.checkpoint2.ui.theme.Checkpoint2Theme
 
 class MainActivity : ComponentActivity() {
@@ -51,6 +51,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GamesScreen(modifier: Modifier = Modifier) {
+    var searchTextState by remember { mutableStateOf("") }
+    var gamesListState by remember { mutableStateOf(getAllGames()) }
+
     Column(modifier = modifier.padding(16.dp)) {
         Text(
             text = "Meus jogos favoritos",
@@ -59,12 +62,12 @@ fun GamesScreen(modifier: Modifier = Modifier) {
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = searchTextState,
+            onValueChange = { searchTextState = it },
             modifier = Modifier.fillMaxWidth(),
             label = { Text(text = "Nome do estúdio") },
             trailingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { gamesListState = getGamesByStudio(searchTextState) }) {
                     Icon(
                         imageVector = Icons.Default.Search,
                         contentDescription = ""
@@ -74,7 +77,7 @@ fun GamesScreen(modifier: Modifier = Modifier) {
         )
         Spacer(modifier = Modifier.height(16.dp))
         LazyColumn() {
-            items(getAllGames()) {
+            items(gamesListState) {
                 GameCard(game = it)
             }
         }
